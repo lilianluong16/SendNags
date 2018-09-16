cf0dd4d3ea6c71ac4a287def75ba3cb1db1c0f8d
Lilian <lilianluong16@gmail.com>
2018-09-15 21:16:53 -0400

    recycled cards in reminders activity

--- app/src/main/java/com/lilian/firestore/firestoretest/RemindersActivity.java
+++ app/src/main/java/com/lilian/firestore/firestoretest/RemindersActivity.java
@@ -2,12 +2,35 @@ package com.lilian.firestore.firestoretest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RemindersActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mRecyclerView = (RecyclerView) findViewById(R.id.reminders_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        String[] ds = new String[3];
+        ds[0] = "test";
+        ds[1] = "hi";
+        ds[2] = "what";
+        mAdapter = new RemindersAdapter(ds);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 }
--- app/src/main/java/com/lilian/firestore/firestoretest/RemindersAdapter.java
+++ app/src/main/java/com/lilian/firestore/firestoretest/RemindersAdapter.java
@@ -1,4 +1,48 @@
 package com.lilian.firestore.firestoretest;
 
-public class RemindersAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class RemindersAdapter extends RecyclerView.Adapter<RemindersAdapter.ReViewHolder> {
+    private String[] dataset;
+
+    public static class ReViewHolder extends RecyclerView.ViewHolder{
+        public TextView mTextView;
+        public ReViewHolder(final View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView.findViewById(R.id.re_msg_text);
+        }
+    }
+
+    public RemindersAdapter(String[] ds){
+        dataset = ds;
+    }
+
+    @Override
+    public RemindersAdapter.ReViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        Log.d("Reminders Adapter", Integer.toString(viewType));
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(viewType, parent, false);
+        ReViewHolder vh = new ReViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ReViewHolder holder, int position){
+        holder.mTextView.setText(dataset[position]);
+    }
+
+    @Override
+    public int getItemCount(){
+        return dataset.length;
+    }
+
+    @Override
+    public int getItemViewType(final int position){
+        return R.layout.reminders_card;
+    }
 }
