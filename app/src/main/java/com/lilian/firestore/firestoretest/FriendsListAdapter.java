19f7042a4c9e2f035d2258fd158bd4e015b8aa11
worldsocold34 <emilywangjt@gmail.com>
2018-09-16 05:45:27 -0400

    changed displays from emails -> names

--- app/src/main/java/com/lilian/firestore/firestoretest/FriendsListAdapter.java
+++ app/src/main/java/com/lilian/firestore/firestoretest/FriendsListAdapter.java
@@ -1,5 +1,6 @@
 package com.lilian.firestore.firestoretest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,14 +8,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class FriendsListAdapter extends RecyclerView.Adapter<FriendsListAdapter.ReViewHolder> {
     public static String[] dataset;
     private static final String TAG = "FriendsListAdapter";
 
+    private FirebaseUser mUser;
+
+    private FirebaseFirestore db;
 
     public static class ReViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
@@ -61,7 +69,29 @@ public class FriendsListAdapter extends RecyclerView.Adapter<FriendsListAdapter.
 
     @Override
     public void onBindViewHolder(ReViewHolder holder, int position){
-        holder.mTextView.setText(dataset[position]);
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        final ReViewHolder holder1 = holder;
+        final int position1 = position;
+        db.collection("users")
+                .whereEqualTo("email", dataset[position1])
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                holder1.mTextView.setText(document.get("name") + " ("+dataset[position1]+")");
+                                break;
+                            }
+                        } else {
+
+                        }
+                    }
+                });
+
     }
 
     @Override
--- app/src/main/java/com/lilian/firestore/firestoretest/RequestsAdapter.java
+++ app/src/main/java/com/lilian/firestore/firestoretest/RequestsAdapter.java
@@ -1,5 +1,6 @@
 package com.lilian.firestore.firestoretest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,10 +8,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class RequestsAdapter extends RecyclerView.Adapter<RequestsAdapter.ReViewHolder> {
     public static String[] dataset;
     private static final String TAG = "RequestsAdapter";
+    private FirebaseUser mUser;
 
+    private FirebaseFirestore db;
     public static class ReViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public TextView mTextView;
@@ -57,7 +68,29 @@ public class RequestsAdapter extends RecyclerView.Adapter<RequestsAdapter.ReView
 
     @Override
     public void onBindViewHolder(ReViewHolder holder, int position){
-        holder.mTextView.setText(dataset[position]);
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        final ReViewHolder holder1 = holder;
+        final int position1 = position;
+        db.collection("users")
+                .whereEqualTo("email", dataset[position1])
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                holder1.mTextView.setText(document.get("name") + " ("+dataset[position1]+")");
+                                break;
+                            }
+                        } else {
+
+                        }
+                    }
+                });
+
     }
 
     @Override
